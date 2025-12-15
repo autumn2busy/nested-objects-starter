c544af26e0f43d141faabb61580295fc9b51726a
SonataStark <autumn.s.williams@gmail.com>
2025-12-14 18:35:44 -0800

    Remove webpack alias configuration from next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
-import path from "path";
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config) => {
-        config.resolve.alias["@"] = path.resolve(__dirname, "src");
-        return config;
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
