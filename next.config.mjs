b95c0e529b97a2f63d2a2cd05d30311fc5343311
SonataStark <autumn.s.williams@gmail.com>
2025-12-14 18:39:46 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias["@"] = path.resolve(__dirname, "src");
+        return config;
+    },
+};
 
 export default nextConfig;
