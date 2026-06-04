ed7d4a5caa75aa11e2b467b3441e0ac64c1decda
Autumn Williams <autumn.s.williams@gmail.com>
2026-06-03 20:34:58 -0400

    Defer GTM until idle

--- apps/web-members/app/layout.tsx
+++ apps/web-members/app/layout.tsx
@@ -109,7 +109,7 @@ export default function RootLayout({
       <head>
         <Script
           id="gtm-script"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
